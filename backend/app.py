cf8adf8f342f8e47e88a359babf20294eee86668
imamtagupta <mamtaguptasde@gmail.com>
2023-12-18 22:55:56 +0530

    questions base crud implemented

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,70 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+import json
 
 # Create an instance of the FastAPI class
 app = FastAPI()
+
+
+with open("data.json") as json_file:
+    file_contents = json_file.read()
+
+print(file_contents)
+
+parsed_json = json.loads(file_contents)
+
+questions = parsed_json["sample_question"]
+options = parsed_json["sample_options"]
+questions_options = parsed_json["sample_question_options_association"]
+
+
+class Questions(BaseModel):
+    id: int
+    title: str
+    descriptions: str
+
 
 # Define a basic route
 @app.get('/')
 def read_root():
     return {
         'success': True,
-        'message': 'Hello World'
+        'data': 'Hello World'
         }
+    
+@app.post('/questions')
+def create_questions(req_data: Questions):
+    print(req_data)
+    questions.append(req_data)
+    return req_data
+    
+    
+@app.get('/questions')
+def get_questions():
+    return {
+        'success': True,
+        'data': questions
+        }
+
+# @app.post('/options')
+# def post_options():
+    
+    
+@app.get('/options')
+def get_options():
+    return {
+        'success': True,
+        'data': options
+        }
+
+
+@app.get('/question_options')
+def get_question_options_ass():
+    return {
+        'success': True,
+        'data': questions_options
+        }
+
 
 
 # Run the application using Uvicorn
