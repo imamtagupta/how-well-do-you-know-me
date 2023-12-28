68374f75c216b22354d9d4c1cf05ee0aec0c3176
imamtagupta <mamtaguptasde@gmail.com>
2023-12-28 09:56:23 +0530

    removed print statement

--- backend/app.py
+++ backend/app.py
@@ -127,28 +127,15 @@ def get_friend_answers():
     
 @app.get('/connection_score')
 def get_connection_score(uid:int, fid:int):
-    print(type(user_answers))
-    print(type(friend_answers))
-    print([type(x) for x in friend_answers])
-    
-    
+
     current_user_answers: UserAnswers = [x for x in user_answers if x.uid == uid]
     current_friend_answers: FriendAnswers = [x for x in friend_answers if x.uid == uid and x.fid == fid]
 
-    # current_user_answers: UserAnswers = [x for x in user_answers if x.get('uid') == uid]
-    # current_friend_answers: FriendAnswers = [x for x in friend_answers if x.get('uid') == uid and x.get('fid') == fid]
-
-    print(current_user_answers, current_friend_answers)
-    
     # Create pairs of qid and oid from user_answers and friend_answers
-    # user_qid_oid = {(ua.get('qid'), ua.get('oid')) for ua in current_user_answers}
-    # friend_qid_oid = {(fa.get('qid'), fa.get('oid')) for fa in current_friend_answers}
     user_qid_oid = {(ua.qid, ua.oid) for ua in current_user_answers}
     friend_qid_oid = {(fa.qid, fa.oid) for fa in current_friend_answers}
 
-    print(user_qid_oid, friend_qid_oid)
-
-    # # Count the number of matching pairs of qid and oid
+    # Count the number of matching pairs of qid and oid
     matching_pairs_count = len(user_qid_oid.intersection(friend_qid_oid))
     
     return {
