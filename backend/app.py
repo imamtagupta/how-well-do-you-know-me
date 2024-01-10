04e7048227dc090bf432136614a7718e208d0750
mamta.gupta@hexafoldtech.com <mamta.gupta@hexafoldtech.com>
2024-01-10 12:01:17 +0530

    remove: unnecessary comments

--- backend/app.py
+++ backend/app.py
@@ -69,7 +69,6 @@ class FriendAnswersRequest(BaseModel):
     uid: int
     fid: int 
 
-# Secret key used to encode/decode JWT tokens
 SECRET_KEY = 'do?you?know?me!'
 bearer_scheme = HTTPBearer()
 
@@ -177,11 +176,9 @@ def get_connection_score(uid:int, fid:int, current_user = Depends(verify_jwt_tok
     current_user_answers: UserAnswers = [x for x in user_answers if x.uid == uid]
     current_friend_answers: FriendAnswers = [x for x in friend_answers if x.uid == uid and x.fid == fid]
 
-    # Create pairs of qid and oid from user_answers and friend_answers
     user_qid_oid = {(ua.qid, ua.oid) for ua in current_user_answers}
     friend_qid_oid = {(fa.qid, fa.oid) for fa in current_friend_answers}
 
-    # Count the number of matching pairs of qid and oid
     matching_pairs_count = len(user_qid_oid.intersection(friend_qid_oid))
     
     return {
