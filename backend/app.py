465bf707158917378467e4b3420ce365fcf1e9d2
imamtagupta <mamtag962000@gmail.com>
2024-01-11 22:24:21 +0530

    removed: pycache

--- backend/app.py
+++ backend/app.py
@@ -177,16 +177,13 @@ def get_connection_score(uid:int, fid:int, current_user = Depends(verify_jwt_tok
     current_user_answers: UserAnswers = [x for x in user_answers if x.uid == uid]
     current_friend_answers: FriendAnswers = [x for x in friend_answers if x.uid == uid and x.fid == fid]
 
-    # Create pairs of qid and oid from user_answers and friend_answers
     user_qid_oid = {(ua.qid, ua.oid) for ua in current_user_answers}
     friend_qid_oid = {(fa.qid, fa.oid) for fa in current_friend_answers}
 
-    # Count the number of matching pairs of qid and oid
     matching_pairs_count = len(user_qid_oid.intersection(friend_qid_oid))
     
     return {
-        'success': True,
-        'data': matching_pairs_count
+        'result': matching_pairs_count
     }
 
 
