d9231ab8eb19572721bae4d1c357f9a43d99c637
imamtagupta <mamtag962000@gmail.com>
2024-01-15 10:09:34 +0530

    environment setup

--- backend/database.py
+++ backend/database.py
@@ -8,6 +8,7 @@ SQLALCHEMY_DATABASE_URL = "postgresql://{settings.DB_USER}:{settings.DB_PASSWORD
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
