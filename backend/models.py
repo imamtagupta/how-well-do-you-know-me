49c7ed63d85214a19d11649d5f0e09992b702ef5
imamtagupta <mamtag962000@gmail.com>
2024-01-21 18:59:29 +0530

    options model update

--- backend/models.py
+++ backend/models.py
@@ -9,3 +9,14 @@ class Questions(Base):
     title = Column(String)
     descriptions = Column(String)
     
+
+class Options(Base):
+    
+    __tablename__ = 'option'
+
+    id = Column(INTEGER, primary_key=True, unique=True) 
+    type = Column(String)
+    value = Column(String)
+    
+class QuestionOptionsAssociation(Base):
+    pass
